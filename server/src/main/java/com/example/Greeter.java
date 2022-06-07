67bfdbc3f80e64c365a783e4f6019d2edd0babfc
nihitha <nihitha@gmail.com>
2022-06-07 13:44:31 +0530

    removed all strings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -15,10 +15,6 @@ public class Greeter {
   //TODO: Add javadoc comment
   public String greet(String someone) {
     String s ="Venkat";
-      String s1 ="Venkat";
-        String s2 ="Venkat";
-          String s3 ="Venkat";
-            String s4 ="Venkat";
     return String.format("Hello, %s!", someone);
      
   }
